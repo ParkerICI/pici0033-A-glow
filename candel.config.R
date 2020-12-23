f1f14690d5d483ac6b2401bb065dc86d3c0ce700
christinens <christine.n.spencer@gmail.com>
2020-12-23 14:57:37 +0000

    updated config

--- candel.config.R
+++ candel.config.R
@@ -1,4 +1,4 @@
 list(
   db.name = "porter-0.9", 
-  candel.token = "eyJhbGciOiJIUzI1NiJ9.eyJjYW5kZWxhYnJhL3VzZXItZW1haWwiOiJjc3BlbmNlckBwYXJrZXJpY2kub3JnIiwiY2FuZGVsYWJyYS9leHBpcmF0aW9uIjoiVGh1IEp1bCAwOSAxNToxMToyNCBVVEMgMjAyMCJ9.llTqEKEJOLerc9vJBPS0BFXQ6ZKHz9CEp0cxDpPDK5EC02XC292JHD2"
+  candel.token = "eyJhbGciOiJIUzI1NiJ9.eyJjYW5kZWxhYnJhL3VzZXItZW1haWwiOiJjc3BlbmNlckBwYXJrZXJpY2kub3JnIiwiY2FuZGVsYWJyYS9leHBpcmF0aW9uIjoiRnJpIEphbiAyMiAxNDo0ODoyOCBVVEMgMjAyMSJ9.B9Dciiry3mdEbgzDt5zJ3Okd0SElkhye3avMgQfMMRI"
 )
